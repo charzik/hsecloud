7ac1f275ee4a08583749842b8cd1d5415d0f441e
charzik <misha-char@mail.ru>
2021-01-24 21:01:42 +0300

    work

--- service_status/handlers/views.py
+++ service_status/handlers/views.py
@@ -2,30 +2,38 @@ import socket
 
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.db import DatabaseError
 
 from . import models
 
 
 @api_view(['GET'])
 def healthcheck(request):
-    models.Item.objects.update_or_create(
-        ip=socket.gethostname(), 
-        status='AVAILABLE'
-    )
-    items = models.Item.objects.all()
-    services = []
-    for item in items:
-        service = {
-            'ip': item.ip,
-            'status': item.status
-        }
-        services.append(service)
+    try:
+        models.Item.objects.update_or_create(
+            ip=socket.gethostname(), 
+            status='AVAILABLE'
+        )
+        items = models.Item.objects.all()
+        services = []
+        for item in items:
+            service = {
+                'ip': item.ip,
+                'status': item.status
+            }
+            services.append(service)
 
-    return Response(
-        status=200, 
-        data={
-            'ip': socket.gethostname(), 
-            'services': services
-        }
-    )
-
+        return Response(
+            status=200, 
+            data={
+                'ip': socket.gethostname(), 
+                'services': services
+            }
+        )
+    except DatabaseError:
+        return Response(
+            status=500, 
+            data={
+                'error': 'Databas is unavalable'
+            }
+        )
